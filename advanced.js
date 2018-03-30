9f2cc9539fea634afc5c110ffcace5a01bcf526d
Student <student@Admins-MacBook-Air-4.local>
2018-03-29 18:30:26 -0700

    speach text

--- advanced.js
+++ advanced.js
@@ -5,7 +5,7 @@
     var commands = {
       'hello': function() {
 var RFhello = ["why","hello", "hey", "hi"];
-var msg = new SpeechSynthesisUtterance(RFhello[2]);
+var msg = new SpeechSynthesisUtterance(RFhello[0]);
 window.speechSynthesis.speak(msg);
 //        alert('Hello world!');
       },
